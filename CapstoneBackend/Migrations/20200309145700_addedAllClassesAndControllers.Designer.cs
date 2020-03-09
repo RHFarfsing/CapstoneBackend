b67179db15b300010e3fc5db5469661a17e70853
Robert Farfsing <robertfarfsing@gmail.com>
2020-03-09 11:03:25 -0400

    added all classes and constuctors

--- CapstoneBackend/Migrations/20200309145700_addedAllClassesAndControllers.Designer.cs
+++ CapstoneBackend/Migrations/20200309145700_addedAllClassesAndControllers.Designer.cs
@@ -3,14 +3,16 @@ using CapstoneBackend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CapstoneBackend.Migrations
 {
     [DbContext(typeof(CapstoneDbContext))]
-    partial class CapstoneDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200309145700_addedAllClassesAndControllers")]
+    partial class addedAllClassesAndControllers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CapstoneBackend/Migrations/20200309145700_addedAllClassesAndControllers.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CapstoneBackend.Migrations
+{
+    public partial class addedAllClassesAndControllers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_User_Username",
+                table: "Users",
+                newName: "IX_Users_Username");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(maxLength: 80, nullable: false),
+                    Justification = table.Column<string>(maxLength: 80, nullable: false),
+                    RejectionReason = table.Column<string>(maxLength: 80, nullable: true),
+                    DeliveryMode = table.Column<string>(maxLength: 20, nullable: false, defaultValue: "Pickup"),
+                    Status = table.Column<string>(maxLength: 10, nullable: false, defaultValue: "New"),
+                    Total = table.Column<decimal>(type: "decimal(11,2)", nullable: false, defaultValue: 0m),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requests_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(maxLength: 30, nullable: false),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Address = table.Column<string>(maxLength: 30, nullable: false),
+                    City = table.Column<string>(maxLength: 30, nullable: false),
+                    State = table.Column<string>(maxLength: 2, nullable: false),
+                    Zip = table.Column<string>(maxLength: 5, nullable: false),
+                    Phone = table.Column<string>(maxLength: 12, nullable: true),
+                    Email = table.Column<string>(maxLength: 225, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartNbr = table.Column<string>(maxLength: 30, nullable: false),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(11,2)", nullable: false),
+                    Unit = table.Column<string>(maxLength: 30, nullable: false),
+                    VendorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Vendors_VendorId",
+                        column: x => x.VendorId,
+                        principalTable: "Vendors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requestlines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequestId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false),
+                    Quantity = table.Column<int>(type: "decimal(11,2)", nullable: false, defaultValue: 1)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requestlines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Requestlines_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Requestlines_Requests_RequestId",
+                        column: x => x.RequestId,
+                        principalTable: "Requests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_VendorId",
+                table: "Products",
+                column: "VendorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requestlines_ProductId",
+                table: "Requestlines",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requestlines_RequestId",
+                table: "Requestlines",
+                column: "RequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_UserId",
+                table: "Requests",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendors_Code",
+                table: "Vendors",
+                column: "Code",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requestlines");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.DropTable(
+                name: "Vendors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_Username",
+                table: "User",
+                newName: "IX_User_Username");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+        }
+    }
+}
