70c7302ddaa3432b5bc9913be60834883bc3f1c8
Robert Farfsing <robertfarfsing@gmail.com>
2020-03-09 12:29:27 -0400

    Added PhotoPath to Product and took out the decimal type in Requestline.Quanity

---
+++ CapstoneBackend/Migrations/20200309160504_addedPhotoPathToProduct.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CapstoneBackend.Migrations
+{
+    public partial class addedPhotoPathToProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPath",
+                table: "Products",
+                maxLength: 225,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoPath",
+                table: "Products");
+        }
+    }
+}
