03924ba5b3e64d01a7e331e36cff06d7f0c09887
Robert Farfsing <robertfarfsing@gmail.com>
2020-03-30 11:58:10 -0400

    Changes address to street

--- CapstoneBackend/Models/Vendor.cs
+++ CapstoneBackend/Models/Vendor.cs
@@ -10,7 +10,7 @@ namespace CapstoneBackend.Models {
         public int Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
-        public string Address { get; set; }
+        public string Street { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
