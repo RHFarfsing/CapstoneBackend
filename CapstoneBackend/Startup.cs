01b40ad32e6d93114db95f9f4b23a58421538bd1
Robert Farfsing <robertfarfsing@gmail.com>
2020-04-13 11:29:20 -0400

    fixed startup

--- CapstoneBackend/Startup.cs
+++ CapstoneBackend/Startup.cs
@@ -25,8 +25,7 @@ namespace CapstoneBackend {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddControllers();
-
+ 
             services.AddDbContext<CapstoneDbContext>(options => {
                 options.UseLazyLoadingProxies();
                 options.UseSqlServer(Configuration.GetConnectionString("CapstoneDb"));
